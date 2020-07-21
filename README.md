e214f07a3778b74bfd175f39c5906205723db15a
alejomontoya <montoyaa241@gmail.com>
2020-07-21 03:09:19 -0500

    Firts commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# A Simple SPA algorithm with vanilla JS
---
+++ router.js
@@ -0,0 +1,43 @@
+class Router {
+
+  constructor(routes) {
+    this.routes = routes
+    this._loadInitialRoute()
+  }
+
+  loadRoute(...urlSegs) {
+    const matchedRoute = this._matchUrlToRoute(urlSegs)
+
+    const url = `/${urlSegs.join('/')}`
+    history.pushState({},'this work',url)
+
+    const routerOutElm = document.getElementById('app')
+
+    routerOutElm.innerHTML = matchedRoute.template;
+
+  }
+
+  _matchUrlToRoute(urlSegs) {
+    const matchedRoute = this.routes.find(route => {
+
+      const routePathSegs = route.path.split('/').slice(1)
+
+      if (routePathSegs.length !== urlSegs.length) {
+        return false;
+      }
+
+      return routePathSegs
+             .every((routePathSeg, i) => routePathSeg === urlSegs[i])
+    })
+
+    return matchedRoute
+  }
+  
+  _loadInitialRoute() {
+    const pathNameSplit = window.location.pathname.split('/')
+    const pathSegs = pathNameSplit.length > 1 ? pathNameSplit.slice(1) : ''
+
+    this.loadRoute(...pathSegs)
+
+  }  
+}
